b7e96bcd16073dac3889a86f980570a0f36c9eb0
Javakky <dlixk_qazwx_three@yahoo.co.jp>
2019-06-04 21:21:10 +0900

    17回目までのコード置いた

---
+++ src/main/java/com/github/javakky/oopl/_17/_2/SystemIn2.java
@@ -0,0 +1,12 @@
+package com.github.javakky.oopl._17._2;
+
+public class SystemIn2{
+	public static void main(String[] args){
+		java.util.Scanner s = new java.util.Scanner(System.in);
+		String str;
+		str = s.next();
+		System.out.println(str);
+		str = s.nextLine();
+		System.out.println(str);
+	}
+}
